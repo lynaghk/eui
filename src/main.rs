1a1b3b3273e37990a7a8ba74f6bea8f25be7a054
Kevin J. Lynagh <kevin@keminglabs.com>
2023-08-06 22:35:27 +0100

    EUI: start sketching out schema UI rendering with Rust. Probably should do first pass in cljs.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
-use schemars::{schema_for, JsonSchema};
-
-#[derive(Debug, JsonSchema, postcard::experimental::schema::Schema)]
+#[derive(Debug, postcard::experimental::schema::Schema)]
 pub enum Light {
     Off,
     On(Color),
 }
 
-#[derive(Debug, JsonSchema, postcard::experimental::schema::Schema)]
+#[derive(Debug, postcard::experimental::schema::Schema)]
 pub struct Color {
     r: u8,
     g: u8,
     b: u8,
 }
 
-pub fn main() {
-    let schema = schema_for!(Light);
-    println!("{}", serde_json::to_string_pretty(&schema).unwrap());
+// pub fn main() {
+//     use postcard::experimental::schema::Schema;
+//     println!("{}", serde_json::to_string_pretty(&Light::SCHEMA).unwrap());
+// }
 
-    use postcard::experimental::schema::Schema;
-    dbg!(Light::SCHEMA);
+use wasm_bindgen::prelude::*;
+use web_sys::{Document, HtmlElement};
+
+fn render(document: &Document, body: &HtmlElement) -> Result<(), JsValue> {
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+
+    val.set_text_content(Some("Hello from Rust!"));
+    body.append_child(&val)?;
+    Ok(())
+}
+
+fn main() -> Result<(), JsValue> {
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let body = document.body().unwrap();
+
+    render(&document, &body)?;
+    render(&document, &body)?;
+
+    Ok(())
 }
