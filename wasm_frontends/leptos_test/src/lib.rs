756fd8b1a53ffa1ad8211e87624820f1601ffa78
Kevin J. Lynagh <kevin@keminglabs.com>
2023-12-14 16:56:36 +0100

    WIP: dying from bevy reflect. maybe I should just go back to clojure hacks...

--- wasm_frontends/leptos_test/src/lib.rs
+++ wasm_frontends/leptos_test/src/lib.rs
@@ -1,10 +1,12 @@
-#[derive(Debug, eui::Schema)]
+use eui::bevy_reflect::{self, Reflect, Typed};
+
+#[derive(Debug, eui::Schema, Reflect)]
 pub enum Light {
     Off,
     On(Color),
 }
 
-#[derive(Debug, eui::Schema)]
+#[derive(Debug, eui::Schema, Reflect)]
 pub struct Color {
     r: u8,
     g: u8,
--- wasm_frontends/leptos_test/src/main.rs
+++ wasm_frontends/leptos_test/src/main.rs
@@ -1,6 +1,8 @@
 use leptos::logging::log;
 use leptos::*;
 
+use eui::bevy_reflect::{self, Reflect, TypeInfo, Typed, *};
+
 // #[component]
 // pub fn SimpleCounter(
 //     /// The starting value for the counter
@@ -21,14 +23,65 @@ use leptos::*;
 // }
 
 #[component]
-pub fn render_control(ty: &'static eui::schema::Type) -> impl IntoView {
+pub fn render_control(
+    ty: &'static TypeInfo,
+    #[prop(into)] cb: Callback<Box<dyn Reflect>>,
+) -> impl IntoView {
     log!("{:?}", ty);
 
-    use eui::schema::Type::*;
-    use eui::schema::{Field, NamedVariant};
     match ty {
-        Bool => todo!(),
-        U8 => {
+        TypeInfo::Struct(info) => {
+            // view! { <div class="type" data-type="struct">
+            //         { (0..info.field_len()).iter().map(|idx| {
+
+            // view!{                       <div class="type" data-type="field">
+            //                                                                                               <label> {field.name()} </label>
+            //                                                                                               // <RenderControl ty=ty cb=cb />
+            //                                                                                               </div>}
+            // }
+            //                      ).collect_view()
+
+            //                      }
+            //                      </div>
+            //             }
+            todo!();
+        }
+        TypeInfo::TupleStruct(_) => todo!(),
+        TypeInfo::Tuple(_) => todo!(),
+        TypeInfo::List(_) => todo!(),
+        TypeInfo::Array(_) => todo!(),
+        TypeInfo::Map(_) => todo!(),
+        TypeInfo::Enum(info) => {
+            // let vview = |variant: &VariantInfo| {
+            //     let variant = variant.clone();
+            //     let name = variant.name();
+            //     let click = move |_| {
+            //         let name = variant.name();
+            //         log!("{:?}", variant);
+            //     view! { <button on:click = click> {name} </button> }
+            //         match &variant {
+            //             VariantInfo::Unit(_) => {
+            //                 let x = UnitVariantInfo::new(&name);
+            //                 log!("{:?}", x);
+
+            //             }
+            //             VariantInfo::Struct(_) => todo!(),
+            //             VariantInfo::Tuple(info) => {
+            //                 // let x = TupleVariantInfo::new(&name);
+            //                 // log!("{:?}", x);
+            //             }
+            //         };
+            //     };
+            // };
+
+            // view! {
+            //             <div class="type" data-type="enum">
+            //               { info.iter().map(vview).collect_view() }
+            //             </div>
+            // }
+            todo!()
+        }
+        TypeInfo::Value(info) => {
             view! {
                             <div class="type" data-type="u8">
             <input type="number" min="0" max="255" on:input=move |e| {
@@ -37,87 +90,180 @@ pub fn render_control(ty: &'static eui::schema::Type) -> impl IntoView {
             </div>
                         }
         }
-        U16 => todo!(),
-        U32 => todo!(),
-        U64 => todo!(),
-        I8 => todo!(),
-        I16 => todo!(),
-        I32 => todo!(),
-        I64 => todo!(),
-        F32 => todo!(),
-        F64 => todo!(),
-        String => todo!(),
-        ByteArray => todo!(),
-        Option(_) => todo!(),
-        Unit | UnitVariant | UnitStruct => {
-            view! { <div class="type" data-type="a-unit"></div> }
+    }
+
+    // use eui::schema::Type::*;
+    // use eui::schema::{Field, NamedVariant};
+    // match ty {
+    //     Bool => todo!(),
+    //     U8 => {
+    //         view! {
+    //                         <div class="type" data-type="u8">
+    //         <input type="number" min="0" max="255" on:input=move |e| {
+    //         log!("{:?}", event_target_value(&e))
+    //         }/>
+    //         </div>
+    //                     }
+    //     }
+    //     U16 => todo!(),
+    //     U32 => todo!(),
+    //     U64 => todo!(),
+    //     I8 => todo!(),
+    //     I16 => todo!(),
+    //     I32 => todo!(),
+    //     I64 => todo!(),
+    //     F32 => todo!(),
+    //     F64 => todo!(),
+    //     String => todo!(),
+    //     ByteArray => todo!(),
+    //     Option(_) => todo!(),
+    //     Unit | UnitVariant | UnitStruct => {
+    //         view! { <div class="type" data-type="a-unit"></div> }
+    //     }
+    //     NewtypeStruct(_) => todo!(),
+    //     NewtypeVariant(_) => todo!(),
+    //     Seq(_) => todo!(),
+    //     Tuple(_) => todo!(),
+    //     TupleStruct(_) => todo!(),
+    //     TupleVariant(variants) => {
+    //         view! { <div class="type" data-type="tuple-variant">
+    //                  { variants.into_iter().map(|ty| view!{<RenderControl ty=ty />}
+
+    //                  ).collect_view()
+
+    //                  }
+    //                  </div>
+
+    //         }
+    //     }
+    //     Map { key, val } => todo!(),
+    //     Struct(fields) => {
+    //         view! { <div class="type" data-type="struct">
+    //                  { fields.into_iter().map(|Field{name, ty}| view!{                       <div class="type" data-type="field">
+    //                                                                                           <label> {name.to_string()} </label>
+    //                                                                                           <RenderControl ty=ty />
+    //                                                                                           </div>}
+
+    //                  ).collect_view()
+
+    //                  }
+    //                  </div>
+
+    //         }
+    //     }
+    //     StructVariant(_) => todo!(),
+    //     Enum(variants) => {
+    //         view! { <div class="type" data-type="enum">
+    //                  { variants.into_iter().map(|NamedVariant{name, ty}| {
+    //                      view!{
+    //                          <div class="type" data-type="named-variant">
+    //                              <label> {name.to_string()} </label>
+    //                              <RenderControl ty=ty />
+    //                              </div>
+    //                      }}).collect_view()
+
+    //                  }
+    //                  </div>
+
+    //         }
+    //     }
+    //     NamedType { name, ty } => {
+    //         view! { <div class="type" data-type="named-type">
+    //                  <label> {name.to_string()} </label>
+    //                  <RenderControl ty=ty />
+    //                  </div>
+    //         }
+    //     }
+    // }
+}
+
+#[component]
+pub fn render_control_dyn(
+    mut x: Box<dyn Reflect>,
+    #[prop(into)] cb: Callback<Box<dyn Reflect>>,
+) -> impl IntoView {
+    let ty = x.get_represented_type_info().unwrap();
+    log!("{:?}", ty);
+    // match ty {
+    //     TypeInfo::Struct(info) => {
+    //         let s = x.clone_value();
+    //         s.set_represented_type(&ty);
+
+    //         let idx = 0;
+    //         view! {
+    //         <div>
+    //         </div>
+    //         }
+    //     }
+    //     TypeInfo::TupleStruct(_) => todo!(),
+    //     TypeInfo::Tuple(_) => todo!(),
+    //     TypeInfo::List(_) => todo!(),
+    //     TypeInfo::Array(_) => todo!(),
+    //     TypeInfo::Map(_) => todo!(),
+    //     TypeInfo::Enum(_) => todo!(),
+    //     TypeInfo::Value(info) => view! {<div>{format!("{:?}", info)}</div>},
+    // }
+
+    match x.reflect_mut() {
+        ReflectMut::Struct(s) => {
+            let idx = 0;
+
+            let f = s.field_at_mut(idx).unwrap().clone_value();
+            let name = s.name_at(idx).unwrap().to_string();
+            let local = |x| log!("Struct got: {:?}", x);
+
+            view! {
+                        <div>
+            { name } <RenderControlDyn x=f cb=local />
+                        </div>
+                        }
         }
-        NewtypeStruct(_) => todo!(),
-        NewtypeVariant(_) => todo!(),
-        Seq(_) => todo!(),
-        Tuple(_) => todo!(),
-        TupleStruct(_) => todo!(),
-        TupleVariant(variants) => {
-            view! { <div class="type" data-type="tuple-variant">
-                     { variants.into_iter().map(|ty| view!{<RenderControl ty=ty />}
+        ReflectMut::TupleStruct(_) => todo!(),
+        ReflectMut::Tuple(_) => todo!(),
+        ReflectMut::List(_) => todo!(),
+        ReflectMut::Array(_) => todo!(),
+        ReflectMut::Map(_) => todo!(),
+        ReflectMut::Enum(_) => todo!(),
+        ReflectMut::Value(v) => match v.get_represented_type_info().unwrap().type_path() {
+            "u64" => {
+                view! {
+                <div class="type" data-type="u8">
+                <input type="number" min="0" max="255" on:input=move |e| {
+                 let new = event_target_value(&e);
+                  cb(Box::new(new))
 
-                     ).collect_view()
-
-                     }
-                     </div>
-
-
+                                                 }/>
+                                                 </div>
+                                                             }
             }
-        }
-        Map { key, val } => todo!(),
-        Struct(fields) => {
-            view! { <div class="type" data-type="struct">
-                     { fields.into_iter().map(|Field{name, ty}| view!{                       <div class="type" data-type="field">
-                                                                                              <label> {name.to_string()} </label>
-                                                                                              <RenderControl ty=ty />
-                                                                                              </div>}
 
-                     ).collect_view()
-
-                     }
-                     </div>
-
-
-            }
-        }
-        StructVariant(_) => todo!(),
-        Enum(variants) => {
-            view! { <div class="type" data-type="enum">
-                     { variants.into_iter().map(|NamedVariant{name, ty}| {
-                         view!{
-                             <div class="type" data-type="named-variant">
-                                 <label> {name.to_string()} </label>
-                                 <RenderControl ty=ty />
-                                 </div>
-                         }}).collect_view()
-
-                     }
-                     </div>
-
-
-            }
-        }
-        NamedType { name, ty } => {
-            view! { <div class="type" data-type="named-type">
-                     <label> {name.to_string()} </label>
-                     <RenderControl ty=ty />
-                     </div>
-            }
-        }
+            unknown => view! { <div> "Unknown type: " { unknown } </div> },
+        },
     }
 }
 
 pub fn main() {
-    use eui::schema::Schema;
+    // use eui::schema::Schema;
+    // log!("{:?}", leptos_test::Light::SCHEMA);
 
-    leptos::logging::log!("{:?}", leptos_test::Light::SCHEMA);
+    let cb = |x| {
+        log!("{:?}", x);
+    };
+    //let ty = leptos_test::Light::type_info();
 
-    mount_to_body(|| view! { <RenderControl ty=leptos_test::Light::SCHEMA  /> });
+    #[derive(Debug, Reflect)]
+    struct Foo {
+        x: u64,
+    }
+
+    let ty = Foo::type_info();
+
+    //mount_to_body(move || view! { <RenderControl ty=ty cb=cb /> });
+
+    mount_to_body(move || {
+        let x = Box::new(Foo { x: 100 });
+        view! { <RenderControlDyn x=x cb=cb /> }
+    });
 
     // mount_to_body(|| {
     //     view! {
